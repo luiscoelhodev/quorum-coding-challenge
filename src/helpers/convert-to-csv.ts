62ab8081df1b51471692d07900da5517b4eba50f
Luis Coelho <mancoeluis@gmail.com>
2022-12-06 23:48:20 -0800

    refactor: separate function with single responsibility

---
+++ src/helpers/convert-to-csv.ts
@@ -0,0 +1,6 @@
+export function convertToCSV(arr: any[]) {
+    const array = [Object.keys(arr[0])].concat(arr)
+    return array.map(it => {
+      return Object.values(it).toString()
+    }).join('\n')
+}
